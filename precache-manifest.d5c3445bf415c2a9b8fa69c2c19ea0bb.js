40d6f4fa0002b2a1a3cf52068e6a95523a3b8639
Johan <johan225@hotmail.es>
2019-11-26 13:27:00 -0500

    Updates

---
+++ precache-manifest.d5c3445bf415c2a9b8fa69c2c19ea0bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff16b665f964e1ef57d8f969a4171fdf",
+    "url": "/votes-react/index.html"
+  },
+  {
+    "revision": "e95b228d487dc549cf9e",
+    "url": "/votes-react/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "454c9a7c60b335616462",
+    "url": "/votes-react/static/css/main.ef388e7a.chunk.css"
+  },
+  {
+    "revision": "e95b228d487dc549cf9e",
+    "url": "/votes-react/static/js/2.f036985a.chunk.js"
+  },
+  {
+    "revision": "454c9a7c60b335616462",
+    "url": "/votes-react/static/js/main.16c479f2.chunk.js"
+  },
+  {
+    "revision": "9a456718f1d47a557a95",
+    "url": "/votes-react/static/js/runtime-main.02d89dec.js"
+  }
+]);
